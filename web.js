342556c6f96c8c609d85ad2e2e8bc98c5c7839f8
vinay <vbvyas@gmail.com>
2013-07-11 00:47:44 -0700

    Fetching contents from index.html using fs module

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
-var app = express.createServer(express.logger());
+var app = express();
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  var data = fs.readFileSync('index.html');
+  response.send(data.toString());
 });
 
 var port = process.env.PORT || 5000;
