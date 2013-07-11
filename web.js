84afa073e6e724546770d77749ebe1c567d7d832
vinay <vbvyas@gmail.com>
2013-07-11 00:57:12 -0700

    Convert back to using createrServer as it was failing in Heroku

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 
-var app = express();
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var data = fs.readFileSync('index.html');
